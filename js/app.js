649371c951f9d9b00876420237c949a2c34e6ee8
janetquispeu@gmail.com <janetquispeu@gmail.com>
2016-09-09 12:05:52 -0500

    readme

--- js/app.js
+++ js/app.js
@@ -1,17 +1,28 @@
-var n = parseInt(prompt("Inserte numero"));
- 
-if (n % 2 == 0){
-	if (n>=2  &&  n<= 5){
-		document.write("Good");
-	}
-	else if (n>=6   &&  n<= 20){
-		document.write("Great");
-	}
-	else if (n > 20 && n<=100){
-		document.write("Perfect");
-	}
-}
-else {
-	document.write("Odd");
-}
+window.addEventListener("load", function() {
+	var resultado=document.getElementById('resultado');
+	resultado.addEventListener("click",function(){
+    	var nombre=document.getElementById('nombre').value;
+    	var edad=parseInt(document.getElementById('edad').value);
+    	var sexo=document.getElementById('sexo').value;
+    	var imprimir=document.getElementById('imprimir');
+    	var persona = new EntidadPersona(nombre, edad, sexo);
+    	imprimir.innerHTML=persona.datos();
+	});
+    function EntidadPersona(nombre,edad,sexo){
+    	this.nombre=nombre;
+    	this.edad= edad;
+    	this.sexo= sexo;
+    	this.mayor_edad=(this.edad >=18);
+    	this.datos= function(){
+    		if(this.mayor_edad){			
+    			 return " Hola, mi nombre es"+ this.nombre+ ", "+ "tengo "+ this.edad+ " años y soy mayor de edad"; 
+    		}else{
+   	
+    			 return " Hola, mi nombre es"+ this.nombre+ ", "+ "tengo "+ this.edad+ " años y soy menor de edad";  
+    		}
+    	};
+    	
+    }      
+});
+
 
